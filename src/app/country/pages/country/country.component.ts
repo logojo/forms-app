d80d4d403d8dc2bc027b2c8fe3a355f52c89624d
logojo <joel_88_7@hotmail.com>
2025-06-18 10:32:06 -0600

    practiva con formularios reactivos terminada

--- src/app/country/pages/country/country.component.ts
+++ src/app/country/pages/country/country.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country',
-  imports: [],
+  imports: [ ReactiveFormsModule, JsonPipe ],
   templateUrl: './country.component.html',
   styleUrl: './country.component.css'
 })
 export class CountryComponent {
+ fb = inject( FormBuilder );
+ countryService = inject( CountryService );
+
+ regions = signal( this.countryService.regions )
+
+ countries = signal<Country[]>([]);
+ borders = signal<Country[]>([]);
+
+ countryForm : FormGroup = this.fb.group({
+  region: ['', Validators.required],
+  country: ['', Validators.required],
+  border: ['', Validators.required],
+ });
+
+onFormChanged = effect( ( onCleanup) => {
+  const regionSubscription = this.onRegionChange();
+  const countrySubscription = this.onCountryChange();
+
+  onCleanup(() => {
+    regionSubscription.unsubscribe();
+    countrySubscription.unsubscribe();
+  });
+
+});
+
+
+onRegionChange() {
+  return this.countryForm.get('region')!.valueChanges
+                          .pipe(
+                            tap(() => this.countryForm.get('country')!.setValue('')),
+                            tap(() => this.countryForm.get('border')!.setValue('')),
+                            tap(() => {
+                              this.borders.set([]);
+                              this.countries.set([]);
+                            }),
+                            switchMap( region  => this.countryService.countriesByRegios( region) )
+                          )
+                          .subscribe( countries => this.countries.set(countries))
+}
+
+onCountryChange() {
+  
+  return this.countryForm.get('country')!.valueChanges
+              .pipe(
+               tap(() => this.countryForm.get('border')!.setValue('')),
+               filter(( value) => value.length > 0),
+               switchMap( (alphaCode)  => this.countryService.countryByAlphaCode( alphaCode ) ),
+               switchMap( ( country )  => this.countryService.countryBorderByCodes( country!.borders ) )
+              )
+             .subscribe(( countries ) => this.borders.set(countries))
+}
+
+
+
 
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Observable, of } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({
@@ -27,14 +27,25 @@ export class CountryService {
   countriesByRegios( region: string) : Observable<Country[]>{
     if(!region) return of([]);
 
-    return this.http.get<Country[]>(`${this.baseUrl}/${region}?fields=cca3,name,borders`);
+    return this.http.get<Country[]>(`${this.baseUrl}/region/${region}?fields=cca3,name,borders`);
   }
 
   countryByAlphaCode(alphaCode: string) : Observable<Country> {
     return this.http.get<Country>(`${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`);
   } 
 
-  countryBorderByCodes( borders: string[] ) {
-    //todo: por hacer
+  countryBorderByCodes( borders: string[] ) : Observable<Country[]> {
+    if( !borders || borders.length === 0 ) return of([]);
+    
+    const countryRequest: Observable<Country>[] = [];
+
+    borders.forEach( code => {
+      const request = this.countryByAlphaCode(code);
+      countryRequest.push(request);
+    });
+    
+
+    return combineLatest( countryRequest ); //*combineLatest combina todas la peticiones del arreglo y se espera a que todas sean resueltas
+
   }
 }
